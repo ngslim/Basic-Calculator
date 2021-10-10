9b120ad027a2edef841a4082f50167efb516ff6d
Nguyễn Sĩ Liêm <63135340+ngslim@users.noreply.github.com>
2021-10-10 10:25:09 +0700

    Add files via upload

---
+++ source/script.js
@@ -0,0 +1,59 @@
+function inputValidate(input) {
+  const button = document.getElementById("calc");
+  if (isNaN(input)) {
+    document.getElementById("error-log").innerHTML = "Số đã điền không hợp lệ";
+    button.disabled = true;
+    return;
+  }
+
+  document.getElementById("error-log").innerHTML = "";
+  button.disabled = false;
+}
+
+function handleButtonClicked(input, options) {
+  if (input.a.value == "") {
+    if (input.b.value == "") {
+      document.getElementById("error-log").innerHTML =
+        "Bạn chưa điền cả hai số";
+    } else {
+      document.getElementById("error-log").innerHTML =
+        "Bạn chưa điền số thứ nhất";
+    }
+    return;
+  } else {
+    if (input.b.value == "") {
+      document.getElementById("error-log").innerHTML =
+        "Bạn chưa điền số thứ hai";
+      return;
+    }
+  }
+
+  var result = 0;
+
+  const firstNum = parseFloat(input.a.value);
+  const secondNum = parseFloat(input.b.value);
+
+  const option = options.arithmetic.value;
+
+  switch (option) {
+    case "ADD":
+      result = firstNum + secondNum;
+      break;
+    case "SUB":
+      result = firstNum - secondNum;
+      break;
+    case "MUL":
+      result = firstNum * secondNum;
+      break;
+    case "DIV":
+      result = firstNum / secondNum;
+      break;
+    default:
+      document.getElementById("error-log").innerHTML =
+        "Bạn chưa chọn phép tính";
+      return;
+  }
+
+  input.result.value = result;
+  document.getElementById("error-log").innerHTML = "Tính thành công";
+}
